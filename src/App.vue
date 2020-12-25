4c6cf8e754cad09ced56076a8111449009026dc7
Alex-dj <7956820+alexdj@user.noreply.gitee.com>
2020-12-25 16:34:31 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <son :img="list" />
+  </div>
+</template>
+
+<script>
+import son from "./components/son";
+// import axios from "axios";
+export default {
+  components: {
+    son,
+  },
+  data() {
+    return {
+      list: [
+        { id: 1, src: require("./assets/img/1.jpg") },
+        { id: 2, src: require("./assets/img/2.jpg") },
+        { id: 3, src: require("./assets/img/3.jpg") },
+        { id: 4, src: require("./assets/img/4.jpg") },
+        { id: 5, src: require("./assets/img/5.jpg") },
+        { id: 6, src: require("./assets/img/6.jpg") },
+        { id: 7, src: require("./assets/img/7.png") },
+        { id: 8, src: require("./assets/img/8.png") },
+      ],
+    };
+  },
+  // created() {
+  //   axios
+  //     .get(
+  //       "https://www.fastmock.site/mock/4f60d566644d6646e94d679ea86f9287/aixue8/zpq"
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data.list);
+  //       this.list = res.data.list;
+  //     });
+  // },
+};
+</script>
+
+<style  scoped>
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="item in img" :key="item.id">
+      <img :src="item.src" alt="" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["img"],
+};
+</script>
+
+<style scoped>
+
+ul{
+    width: 1200px;
+    display: flex;
+    list-style: none;
+    flex-flow: wrap;
+    justify-content: space-around;
+    margin: 100px auto;
+}
+</style>
